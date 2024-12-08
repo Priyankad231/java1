55dd6ff878a03111d6a4e625cf93c8698678510d
Priyankad231 <priyanka.d2628@gmail.com>
2024-12-08 19:38:17 +0530

    third.java

---
+++ third.java
@@ -0,0 +1,12 @@
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        int number = 5;  // You can set this to any number you want to check
+
+        // Check if the number is even or odd
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
+}
